765b3747091f2e9c8f95229502abe7b1a781e2ac
Sergey Pimenov <sergey@pimenov.com.ua>
2020-12-03 15:43:10 +0200

    updated readme

--- README.md
+++ README.md
@@ -2,3 +2,66 @@
 
 Datetime.js is a minimalist JavaScript library that parses, validates, manipulates, and displays dates and times for modern browsers with comfortable modern API.
 
+* 🕒 Familiar Moment.js API & patterns
+* 🔥 Chainable
+* 🌐 I18n support
+* 📦 3kb mini library
+* 👫 All browsers supported
+
+## Getting started
+
+### Documentation
+
+You can find for more details, API, and other docs on [DOCUMENTATION](DOCUMENTATION.md).
+
+### Installation
+
+```console
+npm install @olton/datetime --save
+```
+
+### API
+
+It's easy to use Datetime APIs to parse, validate, manipulate, and display dates and times.
+
+#### Parse
+```javascript
+datetime();
+datetime("2020");
+datetime("2020-12-31");
+datetime("2020-12-31 23:59");
+datetime(2020, 12, 31, 23, 59);
+datetime([2020, 12, 31, 23, 59]);
+Datetime.parse(...);
+Datetime.fromString("16 November 1961 15:24", "dd mm %y h:i", "en")
+Datetime.fromString("16 Ноября 1961 15:24", "dd mm %y h:i", "ru")
+```
+
+#### Display
+```javascript
+datetime().format('{YYYY} MM-DDTHH:mm:ss sss Z A');
+datetime().strftime('{%Y} %n-%dT%H:%M:%S %Q %z %p');
+```
+
+#### Get & set
+```javascript
+datetime().set('month', 3).month();
+datetime().month(3).month();
+```
+
+#### Manipulate
+```javascript
+datetime().add(3, 'day').add(1, 'hour');
+datetime().addDay(3).addHour(1);
+```
+
+#### Align (Start From)
+```javascript
+datetime().align("year"); // Will alignment to 1st Jan of year
+datetime().align("month"); // Will alignment to 1st day of month
+```
+
+#### Compare
+```javascript
+
+```
--- build/datetime.js
+++ build/datetime.js
@@ -656,13 +656,14 @@
             var format = fmt || DEFAULT_FORMAT_STRFTIME;
             var names = global['DATETIME_LOCALES'][locale || this.locale];
             var year = this.year(), year2 = this.year2(), month = this.month(), day = this.day(), weekDay = this.weekDay();
-            var hour = this.hour(), hour12 = this.hour12(), minute = this.minute(), second = this.second(), time = this.time();
+            var hour = this.hour(), hour12 = this.hour12(), minute = this.minute(), second = this.second(), millisecond = this.millisecond(), time = this.time();
             var aDay = lpad(day, "0", 2),
                 aMonth = lpad(month + 1, "0", 2),
                 aHour = lpad(hour, "0", 2),
                 aHour12 = lpad(hour12, "0", 2),
                 aMinute = lpad(minute, "0", 2),
-                aSecond = lpad(second, "0", 2);
+                aSecond = lpad(second, "0", 2),
+                aMillisecond = lpad(millisecond, "0", 3);
 
             var matches = {
                 '%a': names.weekdaysShort[weekDay],
@@ -701,7 +702,10 @@
                 '%Z': this.timezoneName(),
                 '%r': [aHour12, aMinute, aSecond].join(":") + " " + this.ampm(),
                 '%R': [aHour, aMinute].join(":"),
-                "%T": [aHour, aMinute, aSecond].join(":")
+                "%T": [aHour, aMinute, aSecond].join(":"),
+                "%Q": aMillisecond,
+                "%q": millisecond,
+                "%t": this.timezone()
             };
 
             return format.replace(REGEX_FORMAT_STRFTIME, function(match){
