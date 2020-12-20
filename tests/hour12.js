8fac7c6d6b51f93d809b6b8d95c6ff1492ef7373
Sergey Pimenov <sergey@pimenov.com.ua>
2020-12-20 18:13:43 +0200

    added tests for hour12 plugin

--- tests/hour12.js
+++ tests/hour12.js
@@ -11,6 +11,21 @@ describe('Datetime', function() {
         it ('Should be am for 2020 10:00 and true for second argument', () => {
             assert.strictEqual(datetime("2020 10:00").ampm(true), "am");
         })
+        it ('Should be am for 2020 10:00 and true for second argument', () => {
+            assert.strictEqual(datetime("2020 10:00").hour12(), 10);
+        })
+        it ('Should be 11', () => {
+            assert.strictEqual(datetime("2020 10:00").hour12(11).hour(), 11);
+        })
+        it ('Should be 11', () => {
+            assert.strictEqual(datetime("2020 10:00").hour12(11, "pm").hour(), 23);
+        })
+        it ('Should be formated', () => {
+            assert.strictEqual(datetime("2020 10:00").format(), "2020-01-01T10:00:00.000Z");
+        })
+        it ('Should be formated', () => {
+            assert.strictEqual(datetime("2020 10:00").format("YYYY hh:mm"), "2020 10:00");
+        })
     });
 
 })
